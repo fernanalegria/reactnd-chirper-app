1358f39462546fc57c1088899b8cf3a5c20d2d8e
Fernando Alegría <fernanalegria@gmail.com>
2019-01-15 00:06:42 +0100

    feat: Add thunk middleware to support asynchronous action dispatches

--- src/app/state/store.js
+++ src/app/state/store.js
@@ -1,8 +1,9 @@
 import { createStore, combineReducers } from 'redux';
 import * as reducers from './ducks';
+import middleware from './middleware'
 
 export default () => {
   const rootReducer = combineReducers(reducers);
 
-  return createStore(rootReducer);
+  return createStore(rootReducer, middleware);
 };
