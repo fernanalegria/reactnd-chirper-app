b931c98340505e6b19df62f27b378482ea9c58b3
Fernando Alegría <fernanalegria@gmail.com>
2019-01-20 22:21:43 +0100

    feat: Create tweet page

--- src/app/views/App.js
+++ src/app/views/App.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { sharedActions } from '../state/ducks/shared';
 import Dashboard from './pages/Dashboard';
-import NewTweet from './common/NewTweet'
+import NewTweet from './common/NewTweet';
+import TweetPage from './pages/TweetPage';
 import LoadingBar from 'react-redux-loading';
+import { isEmptyObject } from '../utils/helpers';
 
 class App extends Component {
   componentDidMount() {
@@ -14,16 +16,16 @@ class App extends Component {
     return (
       <div>
         <LoadingBar />
-        {this.props.loadingBar && this.props.loadingBar.default === 0 && (
-          <Dashboard />
+        {!this.props.loading && (
+          <TweetPage match={{ params: { id: '2mb6re13q842wu8n106bhk' } }} />
         )}
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ loadingBar }) => ({
-  loadingBar
+const mapStateToProps = ({ users, authedUser }) => ({
+  loading: !authedUser || isEmptyObject(users)
 });
 
 export default connect(mapStateToProps)(App);
